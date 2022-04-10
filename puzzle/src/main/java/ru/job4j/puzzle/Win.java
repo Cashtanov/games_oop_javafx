d4d3c30dbb7eda3f183369c8af5b485cb6dee2ed
github.com <Bkmzpett@gmsil.com>
2022-04-10 11:48:32 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -5,7 +5,7 @@ public class Win {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
             if (board[i][i] == 1) {
-                if ((monoHorizontal(board, i) || monoVertical(board, i))) {
+                if (monoHorizontal(board, i) || monoVertical(board, i)) {
                     result = true;
                     break;
                 }
